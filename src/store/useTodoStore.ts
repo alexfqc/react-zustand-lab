fa3a895432ad9fb47438200424a0567cd6555f3c
Alessander Costa <alexfqc.dev@gmail.com>
2025-07-14 20:10:06 -0300

    TodoState type moved to todo types file

--- src/store/useTodoStore.ts
+++ src/store/useTodoStore.ts
@@ -1,26 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { type Todo } from "../types/todo";
-
-type TodoState = {
-  todos: Todo[];
-  editingId: number;
-  editingValue: string;
-  inputText: string;
-  hydrated: boolean;
-  addTodo: () => void;
-  setTodos: (todos: Todo[]) => void;
-  deleteTodo: (id: number) => void;
-  toggleTodo: (id: number) => void;
-  setEditingId: (id: number) => void;
-  setEditingValue: (value: string) => void;
-  startEditing: (id: number, value: string) => void;
-  saveEdit: () => void;
-  cancelEdit: () => void;
-  setInputText: (text: string) => void;
-  reorderTodos: (fromIndex: number, toIndex: number) => void;
-  setHydrated: (value: boolean) => void;
-};
+import { type Todo, type TodoState } from "../types/todo";
 
 export const useTodoStore = create<TodoState>()(
   persist(
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -3,3 +3,23 @@ export type Todo = {
   title: string;
   completed: boolean;
 };
+
+export type TodoState = {
+  todos: Todo[];
+  editingId: number;
+  editingValue: string;
+  inputText: string;
+  hydrated: boolean;
+  addTodo: () => void;
+  setTodos: (todos: Todo[]) => void;
+  deleteTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+  setEditingId: (id: number) => void;
+  setEditingValue: (value: string) => void;
+  startEditing: (id: number, value: string) => void;
+  saveEdit: () => void;
+  cancelEdit: () => void;
+  setInputText: (text: string) => void;
+  reorderTodos: (fromIndex: number, toIndex: number) => void;
+  setHydrated: (value: boolean) => void;
+};
